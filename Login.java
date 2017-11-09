73ce9c111c7652781644bc82e828bb42ab94a772
darrengodley <darren.godley@Students.ittralee.ie>
2017-11-09 17:43:53 +0000

    09/11/2017

---
+++ Login.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class Login { //Start class
+    private String username;
+    private String password;
+
+    public String getLogin() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setLogin(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    }
+
+} //End class
